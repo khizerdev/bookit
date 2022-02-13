a23bf388926feeb7ae1946857be3480c249f0350
khizerdev <syedkhizerdev@gmail.com>
2022-02-13 18:58:06 +0500

    Creating Redux Store

---
+++ redux/reducers/reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+// import { allRoomsReducer, roomDetailsReducer, newReviewReducer, checkReviewReducer, newRoomReducer, roomReducer, roomReviewsReducer, reviewReducer } from './roomReducers'
+
+// import { authReducer, userReducer, loadedUserReducer, forgotPasswordReducer, allUsersReducer, userDetailsReducer } from './userReducers'
+
+// import { checkBookingReducer, bookedDatesReducer, bookingsReducer, bookingDetailsReducer, bookingReducer } from './bookingReducers'
+
+const reducer = combineReducers({})
+
+export default reducer
