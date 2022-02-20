13072365bb604515a48ef111d92ce9898039cdb9
khizerdev <syedkhizerdev@gmail.com>
2022-02-20 23:23:33 +0500

    Adding Pagination

--- components/Home.js
+++ components/Home.js
@@ -2,33 +2,85 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify';
 import Room from './room/room';
+import Pagination from "react-js-pagination";
+import { useRouter } from 'next/router'
 
 const Home = () => {
 
-    const { rooms , error } = useSelector(state => state.allRooms);
+    const dispatch = useDispatch()
+    const router = useRouter()
+
+    const { rooms , resPerPage, roomsCount, filteredRoomsCount, error } = useSelector(state => state.allRooms);
+
+    let { location, page = 1 } = router.query;
+    page = Number(page)
 
     useEffect(() => {
         toast.error(error)
         // dispatch(clearErrors())
-    }, [])
+    })
+
+    const handlePagination = (pageNumber) => {
+
+        if (location) {
+            let url = window.location.search
+
+            url.includes('&page') ?
+                url = url.replace(/(page=)[^\&]+/, '$1' + pageNumber)
+                :
+                url = url.concat(`&page=${pageNumber}`)
+
+            router.push(url)
+
+        } else {
+
+            router.push(`/?page=${pageNumber}`)
+            // window.location.href = `/?page=${pageNumber}`
+        }
+
+    }
+
+    let count = roomsCount;
+    if (location) {
+        count = filteredRoomsCount
+    }
     return (
-        <section id="rooms" className="container mt-5">
+            <>
 
-      <h2 className='mb-3 ml-2 stays-heading'>Stays in New York</h2>
+                <section id="rooms" className="container mt-5">
 
-      <a href='#' className='ml-2 back-to-search'> <i className='fa fa-arrow-left'></i> Back to Search</a>
-      <div className="row">
-      {rooms && rooms.length === 0 ?
-                        <div className="alert alert-danger mt-5 w-100"><b>No Rooms.</b></div>
-                        :
-                        rooms && rooms.map(room => (
-                            <Room key={room._id} room={room} />
-                        ))
-                    }
+                    <h2 className='mb-3 ml-2 stays-heading'>Stays in New York</h2>
 
-       
-      </div>
-    </section>
+                    <a href='#' className='ml-2 back-to-search'> <i className='fa fa-arrow-left'></i> Back to Search</a>
+                    <div className="row">
+                    {rooms && rooms.length === 0 ?
+                                        <div className="alert alert-danger mt-5 w-100"><b>No Rooms.</b></div>
+                                        :
+                                        rooms && rooms.map(room => (
+                                            <Room key={room._id} room={room} />
+                                        ))
+                                    }
+
+                    
+                    </div>
+                </section>
+                {resPerPage < count &&
+                <div className="d-flex justify-content-center mt-5">
+                    <Pagination
+                        activePage={page}
+                        itemsCountPerPage={resPerPage}
+                        totalItemsCount={roomsCount}
+                        onChange={handlePagination}
+                        nextPageText={'Next'}
+                        prevPageText={'Prev'}
+                        firstPageText={'First'}
+                        lastPageText={'Last'}
+                        itemClass='page-item'
+                        linkClass='page-link'
+                    />
+                </div>
+            }
+            </>
     )
 }
 
